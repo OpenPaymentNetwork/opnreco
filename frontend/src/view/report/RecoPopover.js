a1fbfdd8bc798f26893c89f37576c72ef55deddc
Shane Hathaway <shane@wingcash.com>
2018-11-07 17:25:38 -0700

    Show a table in the reco popover.

--- frontend/src/view/report/RecoPopover.js
+++ frontend/src/view/report/RecoPopover.js
@@ -6,17 +6,52 @@ import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
+import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 import Popover from '@material-ui/core/Popover';
 import Fade from '@material-ui/core/Fade';
 
 
 const styles = theme => ({
-  tabs: {
+  popoverContent: {
+    minWidth: '400px',
+  },
+  titleBar: {
     backgroundColor: theme.palette.primary.main,
     color: '#fff',
+    height: '32px',
+    lineHeight: '32px',
+    padding: '0 16px',
+    fontSize: '1.0rem',
+  },
+  content: {
+    padding: '16px',
+  },
+  table: {
+    borderCollapse: 'collapse',
+    width: '100%',
+  },
+  headCell: {
+    border: '1px solid #bbb',
+    backgroundColor: '#ddd',
+    fontWeight: 'normal',
+  },
+  head2Cell: {
+    border: '1px solid #bbb',
+    fontWeight: 'normal',
+  },
+  spaceRow: {
+    height: '16px',
+  },
+  cell: {
+    border: '1px solid #bbb',
+  },
+  addCell: {
+    border: '1px solid #bbb',
+  },
+  checkCell: {
+    border: '1px solid #bbb',
+    textAlign: 'center',
   },
 });
 
@@ -27,7 +62,6 @@ class RecoPopover extends React.Component {
     dispatch: PropTypes.func.isRequired,
     open: PropTypes.bool,
     anchorEl: PropTypes.object,
-    recoInternal: PropTypes.bool,
   };
 
   constructor(props) {
@@ -39,12 +73,55 @@ class RecoPopover extends React.Component {
     this.props.dispatch(closeRecoPopover());
   }
 
+  renderTable() {
+    const {
+      classes,
+    } = this.props;
+
+    return (
+      <table className={classes.table}>
+        <tbody>
+          <tr>
+            <th colSpan="4" className={classes.headCell}>Movements</th>
+          </tr>
+          <tr>
+            <th width="10%" className={classes.checkCell}>
+              <input type="checkbox" />
+            </th>
+            <th width="25%" className={classes.head2Cell}>Amount</th>
+            <th width="25%" className={classes.head2Cell}>Date and Time</th>
+            <th width="30%" className={classes.head2Cell}>Transfer</th>
+          </tr>
+          <tr>
+            <td colSpan="4" className={classes.addCell}></td>
+          </tr>
+          <tr>
+            <td colSpan="4" className={classes.spaceRow}></td>
+          </tr>
+          <tr>
+            <th colSpan="4"className={classes.headCell}>Account Entries</th>
+          </tr>
+          <tr>
+            <th width="10%" className={classes.checkCell}>
+              <input type="checkbox" />
+            </th>
+            <th width="25%" className={classes.head2Cell}>Amount</th>
+            <th width="25%" className={classes.head2Cell}>Date</th>
+            <th width="30%" className={classes.head2Cell}>Description</th>
+          </tr>
+          <tr>
+            <td colSpan="4" className={classes.addCell}></td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     const {
       classes,
       open,
       anchorEl,
-      recoInternal,
     } = this.props;
 
     return (
@@ -64,15 +141,12 @@ class RecoPopover extends React.Component {
         TransitionComponent={Fade}
       >
         <div className={classes.popoverContent}>
-          <Tabs
-            className={classes.tabs}
-            classes={{indicator: classes.tabsIndicator}}
-            value={recoInternal ? 'internal': 'account'}
-          >
-            <Tab value="account" label="Account" />
-            <Tab value="internal" label="Internal" />
-            <Tab value="remove" label="Remove" />
-          </Tabs>
+          <Typography variant="h6" className={classes.titleBar}>
+            Reconciliation
+          </Typography>
+          <Typography className={classes.content} component="div">
+            {this.renderTable()}
+          </Typography>
         </div>
       </Popover>
     );
